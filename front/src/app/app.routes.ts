1a5a5c42e09af0e389767c416e3a2a6e1aaf4ac4
Laura <laura.saada@efrei.net>
2025-01-17 10:57:03 +0100

    Add element nav-bar + change home page

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -5,7 +5,8 @@ import { RegisterComponent } from './register/register.component';
 import { ProfilComponent } from './profil/profil.component';
 
 export const routes: Routes = [
-    { path: '', component: HomeComponent },
+    { path: '', redirectTo: '/home', pathMatch: 'full' }, // Redirection par défaut vers home
+    { path: 'home', component: HomeComponent },
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
     { path: 'profil', component: ProfilComponent },
