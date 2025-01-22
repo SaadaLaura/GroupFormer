0c61dc4c2e9cd681dad74bcc21094881de0a9900
NakartSaW <ggl.clvr@gmail.com>
2025-01-22 10:24:42 +0100

    create_app and dynamic db_connect

--- backend/api/endpoint/__init__.py
+++ backend/api/endpoint/__init__.py
@@ -1,15 +1,14 @@
 from flasgger import Swagger
 from flask import Flask
 
-from announcements_blueprint import announcements_bp
+from backend.api.endpoint.announcements_blueprint import announcements_bp
 from backend.api.endpoint.projects_blueprint import projects_bp
-from students_blueprint import students_bp
+from backend.api.endpoint.students_blueprint import students_bp
 
-app = Flask(__name__)
-swagger = Swagger(app)
-app.register_blueprint(students_bp, url_prefix='/students')
-app.register_blueprint(announcements_bp, url_prefix='/announcements')
-app.register_blueprint(projects_bp, url_prefix='/projects')
-
-if __name__== "__main__":
-    app.run(debug=True, port=8000)
+def create_app():
+    app = Flask(__name__)
+    Swagger(app)
+    app.register_blueprint(students_bp, url_prefix='/students')
+    app.register_blueprint(announcements_bp, url_prefix='/announcements')
+    app.register_blueprint(projects_bp, url_prefix='/projects')
+    return app
--- backend/api/utils/db_connection.py
+++ backend/api/utils/db_connection.py
@@ -1,7 +1,9 @@
+import os
 import sqlite3
 
-
 def db_connect():
-    # TODO: pas très bien de hardcode le chemin correspond à l'emplacement par rapport à où il est appelé
-    connection = sqlite3.connect('../../database/groupformer.db')
+    base_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../'))
+    db_path = os.path.join(base_dir, 'database', 'groupformer.db')
+
+    connection = sqlite3.connect(db_path)
     return connection
